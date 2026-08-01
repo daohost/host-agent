e33f278b18d5be921b7c818b6dd9fca5ae64202b
Alien Deployer <m@a17.name>
2026-08-01 15:37:34 +0300

    sync labels

--- src/github/github.command.ts
+++ src/github/github.command.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GithubCommand {
-  // constructor(private readonly githubService: GithubService) {}
-  // @Command({
-  //   command: 'sync:labels',
-  // })
-  // async syncLabels(): Promise<void> {
-  //   await this.githubService.syncLabels();
-  // }
+  constructor(private readonly githubService: GithubService) {}
+  @Command({
+    command: 'sync:labels',
+  })
+  async syncLabels(): Promise<void> {
+    await this.githubService.syncLabels();
+  }
 }
